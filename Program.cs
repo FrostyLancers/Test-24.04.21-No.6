4925cc8a17947ff973758c35e676467cb87c1bee
DESKTOP-KC8155E\Lenovo <EasterEggs1230@gmail.com>
2021-04-24 16:12:28 +0700

    Test 24.04.21 (Wongsathorn Sakchatmongkol)

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Test_24._04._21_No._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string letters = Console.ReadLine();
+            int Width = int.Parse(Console.ReadLine());
+            int Height = int.Parse(Console.ReadLine());
+            int Level = int.Parse(Console.ReadLine());
+
+            for (int c = 1; c <= Level; c++)
+            {
+                for (int a = 1; a < Width; a++)
+                {
+                    for (int b = 1; b <= Height; b++)
+                    {
+                        Console.Write(letters);
+                    }
+                    Console.WriteLine(letters);
+                }
+            }
+
+
+        }
+    }
+}
